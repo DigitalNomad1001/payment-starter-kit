8b6d88f26f6d7b98fb8abede8b48ee89fb128804
arjunbhuptani <arjunbhuptani@gmail.com>
2018-10-04 05:10:47 +0400

    prep for component spam

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
+// ***Import the Connext Package here***
 
 import "./App.css";
 
@@ -10,40 +10,19 @@ class App extends Component {
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
-      Contract.setProvider(web3.currentProvider);
-      const instance = await Contract.deployed();
+      // ***Instantiate the Connext Package here***
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      // ***Set web3, accounts and connext instance to state***
+      this.setState({ web3, accounts /*,connext,*/ });
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
+        `Failed to load web3, accounts, or connext. Check console for details.`
       );
       console.log(error);
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.get();
-
-    // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
   };
 
   render() {
@@ -52,9 +31,9 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
+        <h1 className="title">Payment-Starter-Kit</h1>
+        <p>A simple way to test your Connext integration!</p>
+        <h2 className="title">Smart Contract Example</h2>
         <p>
           If your contracts compiled and migrated successfully, below will show
           a stored value of 5 (by default).
