fb4a8bb352405fa42cee93c8a8395095b6104158
Rahul <rksethuram9@gmail.com>
2018-10-04 17:54:53 -0700

    Clean up prod deploy script

--- kernel/deploy.prod.sh
+++ kernel/deploy.prod.sh
@@ -1,23 +1,41 @@
 #!/usr/bin/env bash
 
+# eth settings
 CONTRACT_ADDRESS=0xf25186B5081Ff5cE73482AD761DB0eB0d25abfBF
 ETH_NETWORK_ID=4447
+ETH_NODE_PROTOCOL=http://
+ETH_NODE_HOST=host.docker.internal
+ETH_NODE_PORT=8545
+ETH_NODE_URL=${ETH_NODE_PROTOCOL}${ETH_NODE_HOST}:${ETH_NODE_PORT}
+HUB_ACCOUNT=0x627306090abab3a6e1400e9345bc60c78a8bef57
 
+# image settings
 repository=connextproject
 project=connext
 hub_image=${repository}/${project}_hub:latest
 chainsaw_image=${repository}/${project}_chainsaw:latest
 database_image=postgres:10
-geth_image=ethereum/client-go:latest
+
+# typeorm
+TYPEORM_HOST=postgres
+TYPEORM_PORT=5432
+TYPEORM_DATABASE=$project
+TYPEORM_USERNAME=$project
+TYPEORM_PASSWORD_FILE=/run/secrets/connext_db_dev
 
 # turn on swarm mode if it's not already on
 docker swarm init 2> /dev/null
 
-# always pull in prod mode
-docker pull $database_image
-docker pull $geth_image
-docker pull $hub_image
-docker pull $chainsaw_image
+function pull_if_unavailable {
+    if [[ -z "`docker image ls | grep ${1%:*} | grep ${1#*:}`" ]]
+    then
+        docker pull $1
+    fi
+}
+
+pull_if_unavailable $database_image
+pull_if_unavailable $hub_image
+pull_if_unavailable $chainsaw_image
 
 function new_secret {
     secret=$2
@@ -32,7 +50,7 @@ function new_secret {
     fi
 }
 
-new_secret connext_db_dev
+new_secret connext_db_prod
 
 mkdir -p ~/ethereum
 
@@ -60,12 +78,14 @@ services:
       - connext_db_dev
     environment:
       NODE_ENV: development
-      ENV_VARS_PREFIX: /root/
-      TYPEORM_HOST: postgres
-      TYPEORM_PORT: 5432
-      TYPEORM_DATABASE: $project
-      TYPEORM_USERNAME: $project
-      TYPEORM_PASSWORD_FILE: /run/secrets/connext_db_dev
+      ETH_NODE_URL: $ETH_NODE_URL
+      HUB_ACCOUNT: $HUB_ACCOUNT
+      CONTRACT_ADDRESS: $CONTRACT_ADDRESS
+      TYPEORM_HOST: $TYPEORM_HOST
+      TYPEORM_PORT: $TYPEORM_PORT
+      TYPEORM_DATABASE: $TYPEORM_DATABASE
+      TYPEORM_USERNAME: $TYPEORM_USERNAME
+      TYPEORM_PASSWORD_FILE: $TYPEORM_PASSWORD_FILE
       TYPEORM_LOGGING: "true"
 
   chainsaw:
@@ -74,43 +94,36 @@ services:
       - '3001:3001'
     depends_on:
       - postgres
-      - geth
+      - ganache
     secrets:
       - connext_db_dev
     environment:
       NODE_ENV: development
       ETH_NETWORK_ID: $ETH_NETWORK_ID
-      ETH_NODE_PROTOCOL: http://
-      ETH_NODE_HOST: host.docker.internal
-      ETH_NODE_PORT: 8545
+      ETH_NODE_PROTOCOL: $ETH_NODE_PROTOCOL
+      ETH_NODE_HOST: $ETH_NODE_HOST
+      ETH_NODE_PORT: $ETH_NODE_PORT
       POLLING_INTERVAL: 2000
       CONTRACT_ADDRESS: $CONTRACT_ADDRESS
-      TYPEORM_HOST: postgres
-      TYPEORM_PORT: 5432
-      TYPEORM_DATABASE: $project
-      TYPEORM_USERNAME: $project
-      TYPEORM_PASSWORD_FILE: /run/secrets/connext_db_dev
+      TYPEORM_HOST: $TYPEORM_HOST
+      TYPEORM_PORT: $TYPEORM_PORT
+      TYPEORM_DATABASE: $TYPEORM_DATABASE
+      TYPEORM_USERNAME: $TYPEORM_USERNAME
+      TYPEORM_PASSWORD: $TYPEORM_PASSWORD_FILE
       TYPEORM_LOGGING: "true"
-
-  geth:
-    image: $geth_image
-    ports:
-      - '8545:8545'
-      - '30303:30303'
-    command: ["--rinkeby", "--rpc", "--rpcaddr", "0.0.0.0"]
-    volumes:
-      - ~/ethereum:/root
 
   postgres:
     image: postgres:10
+    ports:
+      - '5432:5432'
     deploy:
       mode: global
     secrets:
       - connext_db_dev
     environment:
-      POSTGRES_USER: $project
-      POSTGRES_DB: $project
-      POSTGRES_PASSWORD_FILE: /run/secrets/connext_db_dev
+      POSTGRES_USER: $TYPEORM_USERNAME
+      POSTGRES_DB: $TYPEORM_DATABASE
+      POSTGRES_PASSWORD_FILE: $TYPEORM_PASSWORD_FILE
     volumes:
       - data:/var/lib/postgresql/data
 EOF
@@ -119,7 +132,7 @@ docker stack deploy -c /tmp/$project/docker-compose.yml $project
 rm -rf /tmp/$project
 
 echo -n "Waiting for the $project stack to wake up."
-number_of_services=4
+number_of_services=3
 while true
 do
     sleep 3
